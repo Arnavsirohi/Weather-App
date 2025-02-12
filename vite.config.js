c32aa87f6ed5772b147ba8291c9baf45672013a7
Arnav sirohi <sirohiarnav3021@gmail.com>
2025-02-12 23:09:28 +0530

    DONE

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/Weather-App/", // Ensure this matches your repo name
+      plugins: [react()],
 })
+
